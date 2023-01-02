db54ba02eb85534472357802c78f4df402b19b21
Branko Daskijević <branko.dask@gmail.com>
2023-01-02 17:55:52 +0100

    exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+  let startNum = Number(start)
+  let endNum = Number(end)
+  let total = 0
 
+  if (startNum < 0 || endNum < 0) return "ERROR"
+  if (!Number.isInteger(start) || !Number.isInteger(end)) return "ERROR"
+
+  if (startNum > endNum) {
+    const temp = startNum
+    startNum = endNum
+    endNum = temp
+  }
+
+  for (let i = startNum; i <= endNum; i++) {
+    total += i
+  }
+
+  return total
 };
 
 // Do not edit below this line
