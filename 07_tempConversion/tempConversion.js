bf6796978fcade96139180c6c4f6e846b81e5ff0
Branko Daskijević <branko.dask@gmail.com>
2023-01-02 18:24:26 +0100

    exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(num) {
+  const result = ((num - 32) * (5/9))
+
+  if (result === 0) return 0
+  if (result % 1 != 0) return Number(result.toFixed(1))
+
+  return result
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(num) {
+  const result = ((num * (9/5) + 32))
+
+  if (result === 0) return 0
+  if (result % 1 != 0) return Number(result.toFixed(1))
+
+  return result
 };
 
 // Do not edit below this line
